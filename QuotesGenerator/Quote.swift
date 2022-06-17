5a7b80549fcffe35e69ddb06a6388987c792eab6
LeeYuseong <82145837+meteor-or@users.noreply.github.com>
2022-06-17 12:01:07 +0900

    명언 생성기

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by lee yuseong on 2022/06/16.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
